b7f968ef7ee7fe595e686c09fd0338a1eba0e182
1-8192 <allegranzia@gmail.com>
2019-03-21 12:05:17 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,23 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = sort_dogs
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def sort_dogs
+    x = Dog.all.sort_by{|dog| dog.employees.count}
+    x.reverse
+  end
 end
